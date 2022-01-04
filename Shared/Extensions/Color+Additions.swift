cc6b17d2bb7e50ea5d649b608456540ca3f02697
Deirdre Saoirse Moen <deirdre@deirdre.net>
2022-01-03 19:00:28 -0800

    Up through Lesson 17.

---
+++ Shared/Extensions/Color+Additions.swift
@@ -0,0 +1,13 @@
+//
+//  Color+Additions.swift
+//  Portfolioish
+//
+//  Created by Deirdre Saoirse Moen on 1/2/22.
+//
+
+import SwiftUI
+
+extension Color {
+	static let systemGroupedBackground = Color(UIColor.systemGroupedBackground)
+	static let secondarySystemGroupedBackground = Color(UIColor.secondarySystemGroupedBackground)
+}
